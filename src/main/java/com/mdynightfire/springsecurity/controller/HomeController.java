37136ee81d9e681be10a86f35a188fc8608cd34b
mdynightfire <644047640@qq.com>
2020-05-25 07:38:44 +0800

    commit

--- src/main/java/com/mdynightfire/springsecurity/controller/HomeController.java
+++ src/main/java/com/mdynightfire/springsecurity/controller/HomeController.java
@@ -1,16 +1,19 @@
 package com.mdynightfire.springsecurity.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * @author mdynightfire
  */
+@Slf4j
 @Controller
 public class HomeController {
 
     @GetMapping("/login")
     public String login() {
+        log.info("login");
         return "login";
     }
 
--- src/main/java/com/mdynightfire/springsecurity/controller/UserController.java
+++ src/main/java/com/mdynightfire/springsecurity/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.mdynightfire.springsecurity.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,11 +11,13 @@ import java.security.Principal;
 /**
  * @author mdynightfire
  */
+@Slf4j
 @Controller
 public class UserController {
 
     @GetMapping("/user")
     public String user(@AuthenticationPrincipal Principal principal, Model model) {
+        log.info("UserController.user");
         model.addAttribute("username", principal.getName());
         return "user/user";
     }
